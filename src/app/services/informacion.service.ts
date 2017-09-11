8d41b0dd26520d0af1fc97486372bb8b1add0458
Jorge Garcia <jorgegargo90@gmail.com>
2017-09-11 18:46:00 +0200

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -7,13 +7,30 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  equipo:any[] = [];
+  cargada_sobre_nosotros:boolean = false;
 
   constructor( public http:Http ) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info() {
     // Para leer el json que hemos creado en assets
     this.http.get("assets/data/info.pagina.json").subscribe( data => {
-      console.log(data.json());
+      //console.log(data.json());
       this.cargada = true;
       this.info = data.json();
+    } );
+  }
+
+  // Esto es con firebase
+  public carga_sobre_nosotros() {
+    // Para leer el json que hemos creado en assets
+    this.http.get("https://paginaweb-9887d.firebaseio.com/equipo.json").subscribe( data => {
+      console.log(data.json());
+      this.cargada_sobre_nosotros = true;
+      this.equipo = data.json();
     } );
   }
 
