ac90b8945b848d2d07df7505e05601577dbc1d33
Jorge Garcia <jorgegargo90@gmail.com>
2017-09-11 20:09:05 +0200

    busqueda finalizada

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -6,6 +6,7 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando_productos:boolean = true;
 
   constructor( public http:Http ) {
@@ -15,18 +16,51 @@ export class ProductosService {
   public cargarProductos() {
       this.cargando_productos = true;
 
-      this.http.get('https://paginaweb-9887d.firebaseio.com/productos_idx.json')
-      .subscribe( data => {
-        // console.log(data.json());
-        // setTimeout(()=>{
-          this.cargando_productos = false;
-          this.productos = data.json();
-        // }, 1500 );
+      let promesa = new Promise ((resolve, reject)=>{
+        this.http.get('https://paginaweb-9887d.firebaseio.com/productos_idx.json')
+        .subscribe( data => {
+          // console.log(data.json());
+          // setTimeout(()=>{
+            this.cargando_productos = false;
+            this.productos = data.json();
+            resolve();
+          // }, 1500 );
+        });
       });
+
+      return promesa;
   }
 
   public cargarProducto( cod:string ) {
     return this.http.get(`https://paginaweb-9887d.firebaseio.com/productos/${cod}.json`);
+  }
+
+  public buscar_producto( termino:string ) {
+    console.log("Buscando producto");
+    console.log(this.productos.length);
+
+    if (this.productos.length === 0) {
+        this.cargarProductos().then(()=>{
+          // termino la carga por el promesa del resolve anterior
+        })
+      } else {
+        this.filtrar_productos(termino);
+      }
+  }
+
+  private filtrar_productos(termino:string){
+
+    this.productos_filtrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+      if (prod.categoria.indexOf(termino) >= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0) {
+          this.productos_filtrado.push(prod);
+          console.log(prod);
+      }
+      //console.log(prod);
+    })
   }
 
 }
