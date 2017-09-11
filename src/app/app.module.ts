362f95229860b876215f3e2fee0d837295c2ec7e
Jorge Garcia <jorgegargo90@gmail.com>
2017-09-11 17:32:43 +0200

    Componentes y rutas agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { WebItemComponent } from './components/web-item/web-item.component';
+
+// Rutas
+import { APP_ROUTING } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    WebItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/index.paginas.ts
@@ -0,0 +1,5 @@
+// Lo usamos para exportar todas las paginas que usamos de forma mas comoda para las rutas
+
+export { AboutComponent } from "./about/about.component";
+export { HomeComponent } from "./home/home.component";
+export { WebItemComponent } from "./web-item/web-item.component";
